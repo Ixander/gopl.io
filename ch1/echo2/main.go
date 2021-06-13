70b59bf9e791639c5904e5e1574eb3f8ca98032d
akaroot <mykolaakaroot@gmail.com>
2021-06-13 04:12:46 +0300

    test dup1

--- ch1/echo2/main.go
+++ ch1/echo2/main.go
@@ -14,12 +14,12 @@ import (
 
 func main() {
 	s, sep := "", ""
-	for i, arg := range os.Args[1:] {
+	for i, arg := range os.Args[0:] {
 		s += sep + arg
 		sep = " "
-		fmt.Println(i)
+		fmt.Println(i, arg)
 	}
-	fmt.Println(s)
+	//fmt.Println(s)
 }
 
 //!-
--- ch1/echo3/main.go
+++ ch1/echo3/main.go
@@ -14,7 +14,7 @@ import (
 //!+
 func main() {
 	//fmt.Println(strings.Join(os.Args[1:], " "))
-	fmt.Println(os.Args[1:])
+	fmt.Println(os.Args[0:])
 }
 
 //!-
