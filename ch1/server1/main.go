e94251f7f0329292dfe7409d73d4a4852e0b6ece
akaroot <mykolaakaroot@gmail.com>
2021-06-19 16:43:58 +0300

    commit from home 190621 web server

--- ch1/server1/main.go
+++ ch1/server1/main.go
@@ -14,13 +14,20 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", handler) // each request calls handler
+	http.HandleFunc("/", handler)         // each request calls handler
+	http.HandleFunc("/test", handlerTest) // each request calls handler
+
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 // handler echoes the Path component of the requested URL.
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func handlerTest(w http.ResponseWriter, r *http.Request) {
+	//fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q\n", "TEST")
 }
 
 //!-
