6ec232dd7a1945442ff02d8f7aa266a39ed1dcee
Ixander <ix.sasha@gmail.com>
2021-07-21 00:23:02 +0300

    learn struct, home

--- Test/struct/main.go
+++ Test/struct/main.go
@@ -5,18 +5,27 @@ import (
 	"time"
 )
 
-func main()  {
+func main() {
 	type Employee struct {
-		ID int
-		Name string
-		Address string
-		Dob time.Time
-		Position string
-		Salary int
+		ID        int
+		Name      string
+		Address   string
+		Dob       time.Time
+		Position  string
+		Salary    int
 		ManagerID int
 	}
 
 	var dilbert Employee
+	dilbert.Salary = 500
+	var employeeOfTheMonth *Employee = &dilbert
+	fmt.Println(employeeOfTheMonth)
+	employeeOfTheMonth.Position += "active member of team"
+	fmt.Println(employeeOfTheMonth)
+	//dilbert.Position = "middle"
+	position := &dilbert.Position
+	*position = "Senior " + *position
 
-	fmt.Println(dilbert)
+	fmt.Println(position, *position)
+
 }
