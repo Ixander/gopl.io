084ffbcaeafc6587105c1b2d161583e454cf5b53
Ixander <ix.sasha@gmail.com>
2021-08-09 21:53:35 +0300

    learn defer, panic, recover; home 090821

--- ch5/sum/main.go
+++ ch5/sum/main.go
@@ -21,13 +21,14 @@ func sum(vals ...int) int {
 
 func main() {
 	//!+main
-	fmt.Println(sum())           //  "0"
-	fmt.Println(sum(3))          //  "3"
-	fmt.Println(sum(1, 2, 3, 4)) //  "10"
+	fmt.Println(sum())               //  "0"
+	fmt.Println(sum(3))              //  "3"
+	fmt.Println(sum(1, 2, 3, 4))     //  "10"
+	fmt.Println(sum(1, 2, 3, 4, 10)) //  "10"
 	//!-main
 
 	//!+slice
-	values := []int{1, 2, 3, 4}
+	values := []int{1, 2, 3, 4, 8}
 	fmt.Println(sum(values...)) // "10"
 	//!-slice
 }
