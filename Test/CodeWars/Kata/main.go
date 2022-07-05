a6118af9d0379abf01de249edc8060782e1964a9
Oleksandr Kyrylov <Aleksandr.Kirilov@privatbank.ua>
2022-07-05 18:01:34 +0300

    add some example

---
+++ Test/CodeWars/Kata/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+type MyString string
+
+func main() {
+	var s MyString
+	s = "ABC"
+	fmt.Println(s.IsUpperCase())
+	fmt.Println(Multiple3And5(12555))
+}
+
+func (s MyString) IsUpperCase() bool {
+	for _, value := range s {
+		if string(value) != strings.ToUpper(string(value)) {
+			return false
+		}
+	}
+	return true
+}
+
+func Multiple3And5(number int) int {
+	res := 0
+	for i := 1; i < number; i++ {
+		if (i%3) == 0 || (i%5) == 0 {
+			res += i
+		}
+	}
+	return res
+}
