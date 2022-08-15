74047917d25202efaa3ae23643c3b4775e92052c
Oleksandr Kyrylov <Aleksandr.Kirilov@privatbank.ua>
2022-08-15 16:17:00 +0300

    from work 150822

--- Test/Practica/map/main.go
+++ Test/Practica/map/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
 	type MyMap map[string]string
@@ -59,4 +64,67 @@ func main() {
 
 	}
 	fmt.Println(sum)
+
+	//Дан массив целых чисел A и целое число k.
+	//Нужно найти и вывести индексы пары чисел, сумма которых равна k.
+	//Если таких чисел нет, то вернуть пустой слайс. Индексы можно вернуть в любом порядке
+
+	var k int
+	fmt.Print("Input k: ")
+	fmt.Scanln(&k)
+
+	slice := make([]int, 0)
+
+	fmt.Println("Input mass: ")
+	input := bufio.NewScanner(os.Stdin)
+
+	for input.Scan() {
+		if input.Text() == "end" {
+			break
+		}
+		inp, _ := strconv.Atoi(input.Text())
+		slice = append(slice, inp)
+	}
+	fmt.Println(slice)
+	fmt.Println(findIndex(slice, k))
+	fmt.Println(find(slice, k))
+
 }
+
+func findIndex(mass []int, k int) []int {
+
+	res := make([]int, 0)
+	data := make(map[int]int)
+	for k, v := range mass {
+		data[k] = v
+	}
+	fmt.Println(data)
+
+	for i := 0; i < len(data); i++ {
+		for j := 0; j < len(data); j++ {
+			if k-data[i] == data[j] && data[i] != data[j] {
+				res = append(res, i)
+			}
+		}
+		fmt.Println(data[i])
+	}
+	return res
+}
+
+func find(arr []int, k int) []int {
+	// Создадим пустую map
+	m := make(map[int]int)
+	// будем складывать в неё индексы массива, а в качестве ключей использовать само значение
+	for i, a := range arr {
+		if j, ok := m[k-a]; ok { // если значение k-a уже есть в массиве, значит, arr[j] + arr[i] = k и мы нашли, то что нужно
+			return []int{i, j}
+		}
+		// если искомого значения нет, то добавляем текущий индекс и значение в map
+		m[a] = i
+	}
+	// не нашли пары подходящих чисел
+	return nil
+}
+
+// как можно заметить, алгоритм пройдётся по массиву всего один раз
+// если бы мы искали подходящее значение каждый раз через перебор массива,
